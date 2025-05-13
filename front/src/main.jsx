f902f9231a5ee625d8b26feff462ac6adf33190b
Mathéo DELAUNAY <dseonay@gmail.com>
2025-05-13 08:56:43 +0200

    [FIX] mise à jour de l'URI de redirection pour l'authentification OIDC

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -11,7 +11,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const oidcConfig = {
   authority: import.meta.env.VITE_OIDC_AUTHORITY,
   client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
-  redirect_uri: "https://hobbit-alpha.vercel.app/",
+  redirect_uri: "https://hobbit-one.vercel.app/",
 };
 
 console.log(oidcConfig);
